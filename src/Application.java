3dc1799da7c0bd0988c902dcf3653484c58b72b1
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2024-02-28 11:00:24 -0300

    refactor: Remove stubs from test cases
    Este commit remove os stubs utilizados para testes. Agora, os testes serão realizados com objetos reais, aumentando a precisão e a confiabilidade dos resultados dos testes.

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,3 @@
-import domain.entity.account.Account;
-import domain.service.AccountService;
 import domain.service.CurrentAccountProcessService;
 import domain.service.SavingsAccountProcessService;
 import domain.usecase.UseCase;
@@ -9,13 +7,6 @@ public class Application {
 
         SavingsAccountProcessService.boot();
         CurrentAccountProcessService.boot();
-
-        Account account = AccountService.getInstance().createSavingsAccount("teste teste", "123.123.123-12");
-        AccountService.getInstance().createDependent(account.getAccountId(), "dependente 1", "000.000.000-00");
-        AccountService.getInstance().createDependent(account.getAccountId(), "dependente 2", "000.000.000-01");
-        
-        Account account1 = AccountService.getInstance().createCurrentAccount("teste teste", "123.123.123-12");
-        AccountService.getInstance().createDependent(account1.getAccountId(), "dependente", "000.000.000-00");
 
         UseCase.boot();
         
